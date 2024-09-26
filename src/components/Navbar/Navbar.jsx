abe36974bfbc6b638c74ccc4a78325ee4702b2e5
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-09-25 19:11:06 -0500

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,15 +4,7 @@ import "./Navbar.css";
 // import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-  return (
-    <div>
-      <ul className="grid grid-cols-12">
-        <li className="bg-blue-600 col-span-6">Inicio</li>
-        <li className="bg-red-600 col-span-3">Registro</li>
-        <li className="bg-green-500 col-span-3">Iniciar sesion</li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Navbar;
