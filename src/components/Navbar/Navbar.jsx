7360d68fd664d66840e8b791d6c7955bf27148a7
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-09-25 22:12:13 -0500

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,3 @@
-import { Button } from "@nextui-org/react";
-import "./Navbar.css";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faUser } from '@fortawesome/free-solid-svg-icons';
-
 const Navbar = () => {
   return <div></div>;
 };
