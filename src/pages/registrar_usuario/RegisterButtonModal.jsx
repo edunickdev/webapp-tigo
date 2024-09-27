3b487f0db2664bf73f6a81de2821383140c05323
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-09-27 10:46:43 -0500

    update

---
+++ src/pages/registrar_usuario/RegisterButtonModal.jsx
@@ -0,0 +1,141 @@
+/* eslint-disable react/prop-types */
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  useDisclosure,
+} from "@nextui-org/react";
+import { IoCreateSharp } from "react-icons/io5";
+import CustomInput from "../../components/custom_input";
+
+const RegisterButtonModal = ({ inputs }) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        isIconOnly
+        onPress={onOpen}
+        className="bg-yellow-500 text-white rounded mx-1"
+      >
+        <IoCreateSharp />
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        size="3xl"
+        className="overflow-y-scroll p-10"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody className="grid grid-cols-12">
+                <h2 className="col-span-12 text-4xl text-gray-700 text-center mb-5">
+                  Edición
+                </h2>
+                <form className="bg-white w-full col-span-6 p-2">
+                  <div className="space-y-4">
+                    <CustomInput item={"Nombre del usuario"} />
+                    <label className="block text-sm font-medium text-gray-700 mt-20">
+                      Propiedad
+                    </label>
+                    <select
+                      className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+                      name="propiedad"
+                      required
+                    >
+                      <option value="" disabled>
+                        Selecciona una opción
+                      </option>
+                      <option value="opcion1">Colsubsidio</option>
+                      <option value="opcion2">PCCOM</option>
+                      <option value="opcion3">Otros</option>
+                    </select>
+
+                    <div>
+                      <label className="block text-sm font-medium text-gray-700">
+                        Modalidad de trabajo
+                      </label>
+                      <select
+                        className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+                        name="modalidad"
+                        required
+                      >
+                        <option value="" disabled>
+                          Selecciona una opción
+                        </option>
+                        <option value="opcion1">Presencial</option>
+                        <option value="opcion2">Teletrabajo Autónomo</option>
+                        <option value="opcion3">
+                          Teletrabajo Suplementario
+                        </option>
+                      </select>
+                    </div>
+
+                    <div>
+                      <label className="block text-sm font-medium text-gray-700">
+                        Facilidades operativas
+                      </label>
+                      <select
+                        className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+                        name="operativas"
+                        required
+                      >
+                        <option value="" disabled>
+                          Selecciona una opción
+                        </option>
+                        <option value="opcion1">Carnet</option>
+                        <option value="opcion2">Tarjeta de acceso</option>
+                        <option value="opcion3">Otros</option>
+                      </select>
+                    </div>
+
+                    <div>
+                      <label className="block text-sm font-medium text-gray-700">
+                        Tipo de contrato del usuario
+                      </label>
+                      <select
+                        className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+                        name="contrato"
+                        required
+                      >
+                        <option value="" disabled>
+                          Selecciona una opción
+                        </option>
+                        <option value="opcion1">Trabajador</option>
+                        <option value="opcion2">Estudiante en práctica</option>
+                        <option value="opcion3">Contratista</option>
+                      </select>
+                    </div>
+                  </div>
+                </form>
+
+                <form className="col-span-6 p-2">
+                  {inputs.map((item) => (
+                    <div key={item.name}>
+                      <label className="block text-sm font-medium text-gray-700">
+                        {item.text}
+                      </label>
+                      <input
+                        type="text"
+                        className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+                        name={item.name}
+                      />
+                    </div>
+                  ))}
+                </form>
+
+                <Button color="primary" className="col-span-12 text-xl mt-4">
+                  Crear
+                </Button>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default RegisterButtonModal;
--- src/pages/registrar_usuario/RegisterUser.jsx
+++ src/pages/registrar_usuario/RegisterUser.jsx
@@ -1,12 +1,15 @@
 import { Button, Input } from "@nextui-org/react";
-import { MdEdit, MdDelete } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 import "./register.css";
 import ButtonModal from "./registerModal";
 import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
-import CustomInput from "../../components/custom_input";
+import RegisterButtonModal from "./RegisterButtonModal";
+import EditButtonModal from "./EditButtonModal";
 
 export const RegisterUser = () => {
+  const [busqueda, setBusqueda] = useState("");
+
   const inputs = [
     { text: "Identificación", name: "cedula" },
     { text: "Celular", name: "celular" },
@@ -21,8 +24,6 @@ export const RegisterUser = () => {
     { text: "Usuario de red", name: "usuario_red" },
   ];
 
-  const [busqueda, setBusqueda] = useState("");
-
   return (
     <div className="flex flex-col items-center max-h-screen bg-gray-100">
       <Input
@@ -32,105 +33,17 @@ export const RegisterUser = () => {
         className="w-96 my-10"
         value={busqueda}
         onValueChange={setBusqueda}
-        endContent={<IoSearchSharp className="h-9 w-7" />}
+        endContent={
+          <Button variant="solid">
+            <IoSearchSharp className="h-9 w-7" />
+          </Button>
+        }
       />
-
       <div className="w-full">
         <div className="grid grid-cols-12 md:flex-row gap-10 col-span-12 shadow-2xl mb-20">
-          <form className="bg-white p-6 rounded-lg shadow-lg w-full col-span-5">
-            <div className="space-y-4">
-              <CustomInput item={"Nombre del usuario"} />
-              <label className="block text-sm font-medium text-gray-700">
-                Propiedad
-              </label>
-              <select
-                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-                name="propiedad"
-                required
-              >
-                <option value="" disabled>
-                  Selecciona una opción
-                </option>
-                <option value="opcion1">Colsubsidio</option>
-                <option value="opcion2">PCCOM</option>
-                <option value="opcion3">Otros</option>
-              </select>
-
-              {inputs.map((item) => (
-                <div key={item.name}>
-                  <label className="block text-sm font-medium text-gray-700">
-                    {item.text}
-                  </label>
-                  <input
-                    type="text"
-                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-                    name={item.name}
-                  />
-                </div>
-              ))}
-
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Modalidad de trabajo
-                </label>
-                <select
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-                  name="modalidad"
-                  required
-                >
-                  <option value="" disabled>
-                    Selecciona una opción
-                  </option>
-                  <option value="opcion1">Presencial</option>
-                  <option value="opcion2">Teletrabajo Autónomo</option>
-                  <option value="opcion3">Teletrabajo Suplementario</option>
-                </select>
-              </div>
-
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Facilidades operativas
-                </label>
-                <select
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-                  name="operativas"
-                  required
-                >
-                  <option value="" disabled>
-                    Selecciona una opción
-                  </option>
-                  <option value="opcion1">Carnet</option>
-                  <option value="opcion2">Tarjeta de acceso</option>
-                  <option value="opcion3">Otros</option>
-                </select>
-              </div>
-
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Tipo de contrato del usuario
-                </label>
-                <select
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-                  name="contrato"
-                  required
-                >
-                  <option value="" disabled>
-                    Selecciona una opción
-                  </option>
-                  <option value="opcion1">Trabajador</option>
-                  <option value="opcion2">Estudiante en práctica</option>
-                  <option value="opcion3">Contratista</option>
-                </select>
-              </div>
-
-              <Button color="primary" className="mt-4">
-                Crear
-              </Button>
-            </div>
-          </form>
-
           {/* Tabla */}
-          <div className="w-full p-4 col-span-7">
+          <div className="col-span-1"></div>
+          <div className="w-full p-4 col-span-10">
             <table className="min-w-full table-auto border-collapse border border-gray-200">
               <thead className="bg-blue-600 text-white">
                 <tr>
@@ -148,13 +61,9 @@ export const RegisterUser = () => {
                   <td className="p-2 border border-gray-200">Nombre</td>
                   <td className="p-2 border border-gray-200">Identificación</td>
                   <td className="p-2 border border-gray-200">
+                    <RegisterButtonModal inputs={inputs} />
                     <ButtonModal />
-                    <Button
-                      isIconOnly
-                      className="bg-green-700 text-white rounded mx-1"
-                    >
-                      <MdEdit className="text-white" />
-                    </Button>
+                    <EditButtonModal inputs={inputs} />
                     <Button
                       isIconOnly
                       className="bg-red-500 text-white rounded"
@@ -167,6 +76,7 @@ export const RegisterUser = () => {
             </table>
           </div>
         </div>
+        <div className="col-span-1"></div>
       </div>
     </div>
   );
