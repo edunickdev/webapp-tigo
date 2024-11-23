424d72cfe1b259c36ac728859349f665c5e739f6
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-11-23 15:21:53 -0500

    fix bug

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Login from "../pages/login/Login.jsx";
@@ -7,28 +8,29 @@ import { RegisterUser } from "../pages/registrar_usuario/RegisterUser.jsx";
 import { CreateActa } from "../pages/crear_acta/CreateActa.jsx";
 import TestPage from "../pages/test.jsx";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import { useUser } from "../context/stores.jsx";
-
+import "react-toastify/dist/ReactToastify.css";
+// import { useUser } from "../context/stores.jsx";
 
 function AppRouter() {
-  
-  const user = useUser((state) => state.user);
+  // const user = useUser((state) => state.user);
 
-  const ProtectRoute = ({ children }) => {
-    return user === !null ? children : <Navigate to={"/"} />;
-  };
+  // const ProtectRoute = ({ children }) => {
+  //   return user === !null ? children : <Navigate to={"/"} />;
+  // };
 
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={
-          <ProtectRoute>
+        <Route
+          path="/home"
+          element={
+            // <ProtectRoute>
             <UserAccount />
-          </ProtectRoute>
-        } />
+            // </ProtectRoute>
+          }
+        />
         <Route path="/buscar" element={<FormSearch />} />
         <Route path="/registrarusu" element={<RegisterUser />} />
         <Route path="/crearacta" element={<CreateActa />} />
--- src/services/equipment_service/equipment_services.jsx
+++ src/services/equipment_service/equipment_services.jsx
@@ -1,14 +1,7 @@
 import axios from "axios";
 
 export const GetEquipmentBySerial = async (serial) => {
-    const response = await axios
-        .get(`http://localhost:8000/api/equipo/${serial}/`)
-        .then((res) => {
-        return res.data;
-        })
-        .catch((err) => {
-        throw err;
-        });
-    return response;
-    }
+  const response = await axios.get(`http://localhost:8000/equipo/${serial}`);
 
+  return response.data;
+};
